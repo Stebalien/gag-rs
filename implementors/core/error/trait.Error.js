1e6c8af9699ff29e61982f7978bc7e61c6719fdb
Steven Allen <steven@stebalien.com>
2015-03-31 16:25:45 -0400

    Update docs for bef63818bc0c4f4b8766aac9e60eab89859e47cc
    Make it work on other unices.
    
    Still doesn't work on windows...

---
+++ implementors/core/error/trait.Error.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['log'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/error/trait.Error.html' title='core::error::Error'>Error</a> for <a class='struct' href='log/struct.SetLoggerError.html' title='log::SetLoggerError'>SetLoggerError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
