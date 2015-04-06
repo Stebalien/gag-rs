c2c53c4dbc2ab603eb7ce52592eb4bc16643d29e
Steven Allen <steven@stebalien.com>
2015-04-06 14:16:16 -0400

    Update docs for fa1a8f6924014accf1f1cbee7c205bfbe46be711
    Forbid mulitple gags.

--- libc/types/os/common/posix01/sidebar-items.js
+++ libc/types/os/common/posix01/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["timezone",""]],"type":[["pthread_t",""],["rlim_t",""],["sighandler_t",""]],"struct":[["glob_t",""],["rlimit",""],["timespec",""],["timeval",""]]});
+initSidebarItems({"struct":[["glob_t",""],["rlimit",""],["timespec",""],["timeval",""]],"type":[["pthread_t",""],["rlim_t",""],["sighandler_t",""]],"enum":[["timezone",""]]});
