55ff318c288962ff1968005c5daa270d1e44eaf2
Steven Allen <steven@stebalien.com>
2015-04-14 16:14:20 -0400

    Update docs for a5370c00b069c941b6253809b0313637ac2d2f6a
    Use tempfile create.
    
    Securely creating temporary files is tricky so I broke it out into a
    second crate.

---
+++ implementors/std/io/trait.Seek.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['tempfile'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Seek.html' title='std::io::Seek'>Seek</a> for <a class='struct' href='tempfile/struct.TempFile.html' title='tempfile::TempFile'>TempFile</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/std/sys/ext/io/trait.AsRawFd.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['tempfile'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/sys/ext/io/trait.AsRawFd.html' title='std::sys::ext::io::AsRawFd'>AsRawFd</a> for <a class='struct' href='tempfile/struct.TempFile.html' title='tempfile::TempFile'>TempFile</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
