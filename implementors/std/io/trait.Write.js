6fa440cc79469c3c0e1a2f573f61ef17745be21b
Steven Allen <steven@stebalien.com>
2015-05-01 15:02:03 -0400

    Update docs for ef681d7b2c3f22f57bfefe522d753d0bdae8dfcd
    Bump to 0.1.6

--- implementors/std/io/trait.Write.js
+++ implementors/std/io/trait.Write.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['tempfile'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Write.html' title='std::io::Write'>Write</a> for <a class='struct' href='tempfile/struct.TempFile.html' title='tempfile::TempFile'>TempFile</a>","<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Write.html' title='std::io::Write'>Write</a> for <a class='struct' href='tempfile/struct.NamedTempFile.html' title='tempfile::NamedTempFile'>NamedTempFile</a>",];
+implementors['tempfile'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Write.html' title='std::io::Write'>Write</a> for <a class='struct' href='tempfile/struct.TempFile.html' title='tempfile::TempFile'>TempFile</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Write.html' title='std::io::Write'>Write</a> for <a class='struct' href='tempfile/struct.NamedTempFile.html' title='tempfile::NamedTempFile'>NamedTempFile</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
