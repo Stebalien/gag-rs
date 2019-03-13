f0bd20673181082b611413230b5f76698ac4992b
Kurt Lawrence <kurt@bne.precisionmining.com>
2019-03-14 01:58:01 +1000

    better eof handling. return eof immediately if peek is empty (don't block)

--- src/windows.rs
+++ src/windows.rs
@@ -33,12 +33,15 @@
 use std::io;
 use winapi;
 use winapi::ctypes::c_void;
-use winapi::shared::{minwindef::DWORD, ntdef::NULL};
+use winapi::shared::{
+    minwindef::{DWORD, LPDWORD},
+    ntdef::NULL,
+};
 use winapi::um::{
-	handleapi::INVALID_HANDLE_VALUE,
-	minwinbase::{OVERLAPPED, SECURITY_ATTRIBUTES},
-	winbase::{STD_ERROR_HANDLE, STD_OUTPUT_HANDLE},
-	winnt::HANDLE,
+    handleapi::INVALID_HANDLE_VALUE,
+    minwinbase::{OVERLAPPED, SECURITY_ATTRIBUTES},
+    winbase::{STD_ERROR_HANDLE, STD_OUTPUT_HANDLE},
+    winnt::HANDLE,
 };
 
 /// Gag type -- stdout.
@@ -49,12 +52,11 @@ pub struct Stderr;
 /// Holds the gag.
 /// Once dropped will return output to the original device.
 pub struct Gag<Io> {
-	write_handle: HANDLE,
-	read_handle: HANDLE,
-	original_handle: HANDLE,
-	std_device: DWORD,
-	eof: bool,
-	mrker: std::marker::PhantomData<Io>,
+    write_handle: HANDLE,
+    read_handle: HANDLE,
+    original_handle: HANDLE,
+    std_device: DWORD,
+    mrker: std::marker::PhantomData<Io>,
 }
 
 /// Gags the stdout stream.
@@ -68,7 +70,7 @@ pub struct Gag<Io> {
 /// println!("and this");
 /// ```
 pub fn stdout() -> io::Result<Gag<Stdout>> {
-	Gag::redirect(STD_OUTPUT_HANDLE)
+    Gag::redirect(STD_OUTPUT_HANDLE)
 }
 
 /// Gags the stderr stream.
@@ -82,26 +84,25 @@ pub fn stdout() -> io::Result<Gag<Stdout>> {
 /// eprintln!("and this");
 /// ```
 pub fn stderr() -> io::Result<Gag<Stderr>> {
-	Gag::redirect(STD_ERROR_HANDLE)
+    Gag::redirect(STD_ERROR_HANDLE)
 }
 
 impl<Io> Gag<Io> {
-	fn redirect(std_device: DWORD) -> io::Result<Self> {
-		let original_handle = get_std_handle(std_device)?;
+    fn redirect(std_device: DWORD) -> io::Result<Self> {
+        let original_handle = get_std_handle(std_device)?;
 
-		let (read_handle, write_handle) = create_pipe()?;
+        let (read_handle, write_handle) = create_pipe()?;
 
-		set_std_handle(std_device, write_handle)?;
+        set_std_handle(std_device, write_handle)?;
 
-		Ok(Gag {
-			write_handle,
-			read_handle,
-			original_handle,
-			std_device,
-			eof: false,
-			mrker: std::marker::PhantomData,
-		})
-	}
+        Ok(Gag {
+            write_handle,
+            read_handle,
+            original_handle,
+            std_device,
+            mrker: std::marker::PhantomData,
+        })
+    }
 }
 
 /// We can say `Gag` is safe as the `HANDLE`s are meant to last until we close them (with `CloseHandle`).
@@ -109,83 +110,99 @@ impl<Io> Gag<Io> {
 unsafe impl<Io> Send for Gag<Io> {}
 
 impl<Io> Drop for Gag<Io> {
-	fn drop(&mut self) {
-		// failures could potentially leak memory, but should be okay with as they are only HANDLE size.
-		unsafe {
-			// failure here could disrupt normal printing
-			winapi::um::processenv::SetStdHandle(self.std_device, self.original_handle);
-		}
-		unsafe {
-			winapi::um::handleapi::CloseHandle(self.read_handle);
-		}
-		unsafe {
-			winapi::um::handleapi::CloseHandle(self.write_handle);
-		}
-	}
+    fn drop(&mut self) {
+        // failures could potentially leak memory, but should be okay with as they are only HANDLE size.
+        unsafe {
+            // failure here could disrupt normal printing
+            winapi::um::processenv::SetStdHandle(self.std_device, self.original_handle);
+        }
+        unsafe {
+            winapi::um::handleapi::CloseHandle(self.read_handle);
+        }
+        unsafe {
+            winapi::um::handleapi::CloseHandle(self.write_handle);
+        }
+    }
 }
 
 impl<Io> io::Read for Gag<Io> {
-	fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-		if self.eof {
-			self.eof = false;
-			return Ok(0); // eof
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        if !has_bytes(self.read_handle)? {
+			return Ok(0);
 		}
-		let buf_len: DWORD = buf.len() as DWORD;
-		let mut bytes_read: DWORD = 0;
 
-		let read_result = unsafe {
-			winapi::um::fileapi::ReadFile(
-				self.read_handle,
-				buf.as_mut_ptr() as *mut c_void,
-				buf_len,
-				&mut bytes_read,
-				NULL as *mut OVERLAPPED,
-			)
-		};
+        let buf_len: DWORD = buf.len() as DWORD;
+        let mut bytes_read: DWORD = 0;
 
-		match read_result {
-			0 => Err(io::Error::last_os_error()),
-			_ => {
-				if bytes_read < buf_len {
-					// read less than the buffer, for pipes this means EOF reached
-					self.eof = true;
-				}
-				Ok(bytes_read as usize)
-			}
-		}
-	}
+        let read_result = unsafe {
+            winapi::um::fileapi::ReadFile(
+                self.read_handle,
+                buf.as_mut_ptr() as *mut c_void,
+                buf_len,
+                &mut bytes_read,
+                NULL as *mut OVERLAPPED,
+            )
+        };
+
+        match read_result {
+            0 => Err(io::Error::last_os_error()),
+            _ => Ok(bytes_read as usize),
+        }
+    }
 }
 
 fn get_std_handle(device: DWORD) -> io::Result<HANDLE> {
-	match unsafe { winapi::um::processenv::GetStdHandle(device) } {
-		INVALID_HANDLE_VALUE => Err(io::Error::last_os_error()),
-		handle => Ok(handle),
-	}
+    match unsafe { winapi::um::processenv::GetStdHandle(device) } {
+        INVALID_HANDLE_VALUE => Err(io::Error::last_os_error()),
+        handle => Ok(handle),
+    }
 }
 
 fn set_std_handle(device: DWORD, handle: HANDLE) -> io::Result<()> {
-	match unsafe { winapi::um::processenv::SetStdHandle(device, handle) } {
-		0 => Err(io::Error::last_os_error()),
-		_ => Ok(()),
-	}
+    match unsafe { winapi::um::processenv::SetStdHandle(device, handle) } {
+        0 => Err(io::Error::last_os_error()),
+        _ => Ok(()),
+    }
 }
 
 /// Returns (read_handle, write_handle).
 fn create_pipe() -> io::Result<(HANDLE, HANDLE)> {
-	let mut read_handle: HANDLE = NULL;
-	let mut write_handle: HANDLE = NULL;
+    let mut read_handle: HANDLE = NULL;
+    let mut write_handle: HANDLE = NULL;
 
-	let create_pipe_result = unsafe {
-		winapi::um::namedpipeapi::CreatePipe(
-			&mut read_handle,
-			&mut write_handle,
-			NULL as *mut SECURITY_ATTRIBUTES,
-			0, // default buffer size
-		)
-	};
+    let create_pipe_result = unsafe {
+        winapi::um::namedpipeapi::CreatePipe(
+            &mut read_handle,
+            &mut write_handle,
+            NULL as *mut SECURITY_ATTRIBUTES,
+            0, // default buffer size
+        )
+    };
 
-	match create_pipe_result {
-		0 => Err(io::Error::last_os_error()),
-		_ => Ok((read_handle, write_handle)),
-	}
+    match create_pipe_result {
+        0 => Err(io::Error::last_os_error()),
+        _ => Ok((read_handle, write_handle)),
+    }
+}
+
+/// Uses PeekNamedPipe and checks TotalBytesAvail
+fn has_bytes(handle: HANDLE) -> io::Result<bool> {
+    let mut bytes_avail: DWORD = 0;
+
+    let result = unsafe {
+        winapi::um::namedpipeapi::PeekNamedPipe(
+            handle,
+            NULL as *mut c_void,
+            0,
+            NULL as LPDWORD,
+            &mut bytes_avail,
+            NULL as LPDWORD,
+        )
+    };
+	
+    if result == 0 {
+        return Err(io::Error::last_os_error());
+    }
+
+    Ok(bytes_avail > 0)
 }
